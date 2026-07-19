102bd7165568819143c473970f9d632f73c1bddb
AklimaKhandaker <akhandaker@ivytech.edu>
2026-07-18 21:26:58 -0400

    Add files via upload

---
+++ Python Assignment 6.py
@@ -0,0 +1,16 @@
+# Setting variable c
+
+c = "cats"
+
+# Setting variable d
+
+d = "dogs."
+
+# Setting variable s using variables c and d, and other related texts
+
+s = "It is raining " + c + " and "  + d
+
+# Printing final outputs
+
+print(s)
+
